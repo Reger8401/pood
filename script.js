604af08a0bcde439de46647fbb3e27d2bc255aa7
Reger8401 <72143354+Reger8401@users.noreply.github.com>
2022-05-22 18:12:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let phrases = [
+  'отправить другу смешную гифку',
+  'посмотреть скидки на авиабилеты',
+  'разобраться, о чём поют рэперы',
+  'Юрий Дудь',
+  'расставить книги на полке по цвету',
+  'читать про зарплаты в Сан-Франциско'
+];
+
+function getRandomElement(arr) {
+  let randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
+}
+document.querySelector('.button');
+let button = document.querySelector('.button');
+let phrase = document.querySelector('.phrase');
+let advice = document.querySelector('.advice');
+let image = document.querySelector('.image');
+
+button.addEventListener('click', function(){
+  phrase.textContent = getRandomElement(phrases);
+  /*console.log('Счастье не за горами');*/
+})
